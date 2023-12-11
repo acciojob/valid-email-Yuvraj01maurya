bc56576055808aa57caeaa33c01506bbd0307e67
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-11 23:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  // Check if the provided email matches the pattern
+  return emailPattern.test(str);
+	
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
